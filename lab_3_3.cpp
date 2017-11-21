79d1d58cb5e2ba5976aa7f76e37fb712ad9c44eb
Unknown <christsutamas@gmail.com>
2017-11-21 18:20:32 +0700

    ทำข้อ 3

--- lab_3_3.cpp
+++ lab_3_3.cpp
@@ -6,10 +6,15 @@ int main() {
 
     const float pi = 3.14315926536;
     float r;
+    float h=15;
 
     cin >> r;
 
     cout << "Area : " << pi * r * r;
+
+    cout << "Area : " << 2 * pi * r;
+
+    cout << "Area : " << pi * r * r * h;
 
   return 0;
 }
