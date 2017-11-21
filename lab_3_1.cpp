36ac5e54bfec3b73af6e3d925cb23ce35bb2b19d
Unknown <christsutamas@gmail.com>
2017-11-21 17:34:40 +0700

    ทำข้อ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+
+  cout << "Name:" << "Mr." << "Sutamas Plodtod" << endl;
+  cout << "Nickname:" << "Christ" << endl;
+  cout << "Sex:" << "Female" << endl;
+  cout << "Birthday:" << "December 12, 1998" << endl;
+  cout << "Hobbies:" << "read a book" << endl;
+  cout << "Status:" << "Single" << endl;
+
   return 0;
 }
